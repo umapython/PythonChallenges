4d353456c7f56824e95d16321a777a0b7ada35df
uma <pdxuma@gmail.com>
2024-02-14 17:23:39 -0800

    final attempt

--- unittest_sample.py
+++ unittest_sample.py
@@ -5,18 +5,12 @@ def add_numbers(a, b):
     return a + b
 
 
-result = add_numbers(3, 5)
-print(result)
 
-
-class TestAddNumbers(unittest.TestCase):
+class AddNumbers(unittest.TestCase):
 
     def test_add_numbers(self):
         print(self.assertEqual(add_numbers(2, 2), 4))
 
-    def test_add_neg_numbers(self):
-        print(self.assertEqual(add_numbers(-2, -2), -4))
 
-x=TestAddNumbers()
-x.test_add_numbers()
-x.test_add_neg_numbers()
+if __name__ == "__main__":
+    unittest.main()
