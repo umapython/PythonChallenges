12c8999391639f612d3966316fcc629279e0c0d6
uma <pdxuma@gmail.com>
2024-02-21 17:20:01 -0800

    testing

--- permute.py
+++ permute.py
@@ -1,12 +1,20 @@
-#Write a program that uses recursion to generate all permutations of a list
-def permute_list(lst,f=0):
-    #base case
-    if f>=len(lst):
-        print(lst)
+def GetPermutations(listOFElements):
+    # Create a empty list of lists
+    listOfLists = []
+    if len(listOFElements) <= 1:
+        # If list has 0 or 1 element, then add in list of lists
+        listOfLists.append(listOFElements)
+    else:
+        # If list has more than 1 elements
+        # Iterate over all elements by index position
+        for i in range(len(listOFElements)):
+            # Select element at ith poistion
+            elem = listOFElements[i]
+            # Select remaining elements
+            remainingList = listOFElements[:i] + listOFElements[i+1:]
+            # get permutations of all remaining elements, and add ith element to it
+            for perms in GetPermutations(remainingList):
+                listOfLists.append([elem] + perms)
 
-    for s in range(f,len(lst)):
-        lst[f],lst[s]=lst[s],lst[f]
-        permute_list(lst,f+1)
-        
-
-permute_list([1,2,3])
+    # return permutations of given list
+    return listOfLists
